afdc588714e467f11c3a9e6019c454a95bede843
OssiHela <ossi.hela@gmail.com>
2021-06-29 18:00:04 +0300

    working aiming

--- Assets/Scripts/Ossi/PlayerBow.cs
+++ Assets/Scripts/Ossi/PlayerBow.cs
@@ -8,15 +8,12 @@ public class PlayerBow : MonoBehaviour
     public Transform aim;
     public GameObject bulletPrefab;
     private Rigidbody bulletRb;
+    public LayerMask mouseAimMask;
 
-    Vector3 lookPos;
+    Vector3 mousePoint;
 
     public float chargeTime;
     public float speed;
-
-	float angle;
-
-   	float aimSpeed;
 
     //private float nextFire = 0f;
 
@@ -39,14 +36,8 @@ public class PlayerBow : MonoBehaviour
     }
 
     void Update() {
-    	if(aimSpeed <= 0.005f) {
-        	aimSpeed += Time.fixedDeltaTime;
-        }
-        else {
-        	Aim();
-        	aimSpeed = 0f;
-        }
 
+    	Aim();
 
     	if(Input.GetButton("Fire1")) {
     		chargeTime += Time.deltaTime;
@@ -80,18 +71,14 @@ public class PlayerBow : MonoBehaviour
     }
 
     void Aim() {
-    	Vector3 mousePosition = GetMouseWorldPosition();
-    	Vector3 aimDirection = (mousePosition - transform.position).normalized;
-        angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+    	Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    	RaycastHit hit;
 
-
-	    //aim.eulerAngles = new Vector3(angle, 90f, 0f);
-
-	    aim.LookAt(Input.mousePosition);
-
-
-        Debug.Log(angle);
-
+    	if(Physics.Raycast(ray, out hit, Mathf.Infinity, mouseAimMask)) {
+    		mousePoint = hit.point;
+    		mousePoint.z = 0;
+    		aim.LookAt(mousePoint);
+    	}
     }
 
     public Vector3 GetMouseWorldPosition() {
